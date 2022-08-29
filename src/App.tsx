c6fdcc150b37b27876cbeb6185d043b04299474f
Paulo Sergio <pauloserg861@gmail.com>
2022-08-29 16:04:25 -0300

    importando cronometro e relogio

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Form } from "./component/Form";
 import List from "./component/List";
 
 import style from './App.module.scss'
+import Chronometer from "./component/Chonometer";
 
 const App = () => {
   return (
     <div className={style.AppStyle}>
       <Form />
       <List />
+      <Chronometer />
     </div>
   );
 }
