f55c44ef5dea775126a3499929bc7f1993a4da50
Paulo Sergio <pauloserg861@gmail.com>
2022-08-29 13:27:29 -0300

    importando lista para o app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Form } from "./component/Form";
+import List from "./component/List";
 
 const App = () => {
   return (
     <div className="App">
       <Form />
+      <List />
     </div>
   );
 }
