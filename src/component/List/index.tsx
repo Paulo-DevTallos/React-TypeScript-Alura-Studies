946107c03bba6e3c786023efff4780a528975623
Paulo Sergio <pauloserg861@gmail.com>
2022-08-29 13:27:11 -0300

    criando componente de lista

---
+++ src/component/List/index.tsx
@@ -0,0 +1,19 @@
+const List = () => {
+  return(
+    <aside> 
+      <h2>Estudos do dia</h2>
+      <ul>
+        <li>
+          <h3>React JS</h3>
+          <span>02:00:00</span>
+        </li>
+        <li>
+          <h3>TypeScript</h3>
+          <span>01:30:00</span>
+        </li>
+      </ul>
+    </aside>    
+  )
+}
+
+export default List
