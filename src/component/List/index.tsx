d9c01423fcaf2dcaa83c7bff20381822b10c7781
Paulo Sergio <pauloserg861@gmail.com>
2022-08-29 16:25:28 -0300

    inplementando state

--- src/component/List/index.tsx
+++ src/component/List/index.tsx
@@ -1,31 +1,23 @@
+import { useState } from 'react'
 import Item from './Item'
 import style from './List.module.scss'
 
 const List = () => {
-  const listRender = [
-    {
-      id: 0,
-      tarefa: "JavaScript",
-      tempo: "02:10:00",  
-    },
-    {
-      id: 1,
-      tarefa: "React typeScript",
-      tempo: "01:15:20"
-    },
-    {
-      id: 1,
-      tarefa: "Todo List",
-      tempo: "01:05:20"
-    }
-  ]
-  
+
+const [tarefa, setTarefa] = useState([{
+  id: 0,
+  tarefa: "tarefa padrao",
+  tempo: "01:02:03"
+}])
+
   return(
     <aside className={style.listaTarefas}> 
-      <h2>Estudos do dia</h2>
+      <h2 onClick={() => {
+        setTarefa([...tarefa, {id: 0, tarefa: "Teste", tempo: "00:00:00"}])
+      }}>Estudos do dia</h2>
       <ul>
         {
-          listRender.map((item, id) => (
+          tarefa.map((item, id) => (
             <Item 
               key={id}
               /* conseguimos utilizar tudo oq está no objeto item porém é necessário ter atenção*/
